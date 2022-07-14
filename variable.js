97b91177d21c00f9dd8d6e39b4db1034d8e4eca1
googlekeep <info.workingforyou@gmail.com>
2022-07-14 19:21:15 +0530

    home completed

---
+++ variable.js
@@ -0,0 +1,15 @@
+// Number
+var watchPrice = 120;
+watchPrice = 125;
+
+// String
+var userName ='Sogir Uddin';
+
+
+// Boolean
+var isExpensive = true;
+
+// integer: 123456 145
+// float 2.7111 5.7788 3.779
+
+
