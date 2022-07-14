b0790a7228f7d69c80027a60062f9f8059c658a3
googlekeep <info.workingforyou@gmail.com>
2022-07-14 19:37:57 +0530

    arry

---
+++ arry.js
@@ -0,0 +1,15 @@
+var friendsAge = [11, 13, 17];
+
+
+var muriChanachurFee = [34, 55, 221, 3];
+
+
+
+var nayikas = ['mahi', 'apu', 'raju'];
+
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels = ['a','e','i', 'o', 'u'];
+
+console.log(nayikas.length);
